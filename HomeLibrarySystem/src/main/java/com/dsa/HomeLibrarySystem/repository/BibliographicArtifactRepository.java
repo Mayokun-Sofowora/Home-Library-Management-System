1b6cc912057676ca18fd986ee2b15a4aa6cef032
Mayokun Sofowora <73405462+Mayokun-Sofowora@users.noreply.github.com>
2024-09-16 21:56:38 +0200

    Delete HomeLibrarySystem/src/main directory

--- HomeLibrarySystem/src/main/java/com/dsa/HomeLibrarySystem/repository/BibliographicArtifactRepository.java
+++
@@ -1,8 +0,0 @@
-package com.dsa.HomeLibrarySystem.repository;
-
-import com.dsa.HomeLibrarySystem.model.BibliographicArtifact;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BibliographicArtifactRepository extends JpaRepository<BibliographicArtifact, Long> {
-    boolean existsByTitle(String title);
-}
--- HomeLibrarySystem/src/main/java/com/dsa/HomeLibrarySystem/repository/ReviewRepository.java
+++
@@ -1,7 +0,0 @@
-package com.dsa.HomeLibrarySystem.repository;
-
-import com.dsa.HomeLibrarySystem.model.Review;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReviewRepository extends JpaRepository<Review, Long> {
-}
